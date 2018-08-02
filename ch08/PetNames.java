c25c081e1c0f96604e19f86d75bf73d05b6dc921
bxsullivan91 <bxsullivan91@gmail.com>
2018-08-02 09:00:44 -0500

    Pet Names

---
+++ ch08/PetNames.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class PetNames
+{
+    public static void main(String[] args)
+    {
+
+        receivePetNames();
+
+    }
+
+    public static void receivePetNames()
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many pets do you have?");
+        int petNumber = input.nextInt();
+
+        String[] petNames = new String[petNumber];
+
+        System.out.println("What are your pets names?");
+        input.nextLine();
+
+        for (int i = 0; i < petNames.length; i++)
+        {
+            petNames[i] = input.nextLine();
+        }
+
+        System.out.println("Your pets name's are: ");
+        int order = 0;
+        for (int j = 0; j < petNames.length; j++)
+        {
+            order = j;
+            System.out.println(j + " " + petNames[j]);
+        }
+    }
+}
